6bad18204282d53ca694b6967f73cd92cfa4d7e1
Sudipta-123 <sudipta3223@gmail.com>
2026-08-29 19:00:17 +0530

    Update project

--- restaurant-backend/index.js
+++ restaurant-backend/index.js
@@ -156,7 +156,7 @@ app.post("/order", async (req, res) => {
 });
 
 // Server start
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
